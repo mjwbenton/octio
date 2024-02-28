d390a95fcaa657fa80e761286a1b403e43b66451
Matthew Benton <m@mattb.tech>
2024-02-28 16:24:12 +0000

    base64 encode credentials

--- ingestion/src/index.ts
+++ ingestion/src/index.ts
@@ -4,7 +4,7 @@ const ELECTRICITY_ENDPOINT = `https://api.octopus.energy/v1/electricity-meter-po
 const GAS_ENDPOINT = `https://api.octopus.energy/v1/gas-meter-points/${env.OCTOPUS_GAS_MPRN}/meters/${env.OCTOPUS_GAS_SERIAL}/consumption/`;
 
 const HEADERS = {
-  Authorization: `Basic ${env.OCTOPUS_API_KEY}`,
+  Authorization: `Basic ${Buffer.from(env.OCTOPUS_API_KEY + ":").toString("base64")}`,
 };
 
 export async function handler() {
@@ -13,7 +13,7 @@ export async function handler() {
   });
   console.log(
     "Electricity: ",
-    JSON.stringify(await electricity.json(), null, 2),
+    JSON.stringify(await electricity.json(), null, 2)
   );
   const gas = await fetch(GAS_ENDPOINT, {
     headers: HEADERS,
