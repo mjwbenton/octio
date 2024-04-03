4a72c73dda40e75b7f3a7c6bd391fcbe4a4eeb0f
Matthew Benton <m@mattb.tech>
2024-04-03 13:39:17 +0100

    Consider paginated dynamo data in graphql

--- graphql/src/data/consumptionData.ts
+++ graphql/src/data/consumptionData.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, paginateQuery } from "@aws-sdk/client-dynamodb";
 import env from "../env";
 import { formatISO } from "date-fns/formatISO";
 import { parseISO } from "date-fns/parseISO";
@@ -20,9 +20,9 @@ export enum EnergyType {
 export async function getConsumptionData(
   energyType: EnergyType,
   startDate: Date,
-  endDate: Date,
+  endDate: Date
 ): Promise<Array<ConsumptionDataPoint>> {
-  const command = new QueryCommand({
+  const command = {
     TableName: env.CONSUMPTION_TABLE,
     KeyConditionExpression:
       "energyType = :energyType and startDate between :start and :end",
@@ -31,16 +31,24 @@ export async function getConsumptionData(
       ":start": { S: formatISO(startDate) },
       ":end": { S: formatISO(endDate) },
     },
-  });
+  };
 
-  const results = await DYNAMO_CLIENT.send(command);
-  console.log("Consumption Data", JSON.stringify(results, null, 2));
-  return (
-    results.Items?.map((val) => ({
-      energyType: val.energyType.S! as EnergyType,
-      startDate: parseISO(val.startDate.S!),
-      endDate: parseISO(val.endDate.S!),
-      consumption: parseFloat(val.consumption.N!),
-    })) ?? []
+  const paginator = paginateQuery(
+    { client: DYNAMO_CLIENT, pageSize: 1000 },
+    command
   );
+
+  const results: Array<ConsumptionDataPoint> = [];
+  for await (const page of paginator) {
+    console.log("Consumption Data Page", JSON.stringify(page, null, 2));
+    page.Items?.forEach((val) => {
+      results.push({
+        energyType: val.energyType.S! as EnergyType,
+        startDate: parseISO(val.startDate.S!),
+        endDate: parseISO(val.endDate.S!),
+        consumption: parseFloat(val.consumption.N!),
+      });
+    });
+  }
+  return results;
 }
--- graphql/src/data/gridData.ts
+++ graphql/src/data/gridData.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, paginateQuery } from "@aws-sdk/client-dynamodb";
 import env from "../env";
 import { formatISO } from "date-fns/formatISO";
 import { parseISO } from "date-fns/parseISO";
@@ -16,9 +16,9 @@ export interface GridDataPoint {
 
 export async function getGridData(
   startDate: Date,
-  endDate: Date,
+  endDate: Date
 ): Promise<Array<GridDataPoint>> {
-  const command = new QueryCommand({
+  const command = {
     TableName: env.GRID_TABLE,
     KeyConditionExpression:
       "postcode = :postcode and startDate between :start and :end",
@@ -27,19 +27,26 @@ export async function getGridData(
       ":start": { S: formatISO(startDate) },
       ":end": { S: formatISO(endDate) },
     },
-  });
+  };
 
-  const results = await DYNAMO_CLIENT.send(command);
-  console.log("Grid Data", JSON.stringify(results, null, 2));
-  return (
-    results.Items?.map((val) => ({
-      startDate: parseISO(val.startDate.S!),
-      endDate: parseISO(val.endDate.S!),
-      intensity: parseInt(val.intensity.N!),
-      mix: val.mix.L!.map((mix) => ({
-        fuel: mix.M!.fuel.S!,
-        percentage: parseFloat(mix.M!.percentage.N!),
-      })),
-    })) ?? []
+  const paginator = paginateQuery(
+    { client: DYNAMO_CLIENT, pageSize: 1000 },
+    command
   );
+  const results: Array<GridDataPoint> = [];
+  for await (const page of paginator) {
+    console.log("Grid Data Page", JSON.stringify(page, null, 2));
+    page.Items?.forEach((val) => {
+      results.push({
+        startDate: parseISO(val.startDate.S!),
+        endDate: parseISO(val.endDate.S!),
+        intensity: parseInt(val.intensity.N!),
+        mix: val.mix.L!.map((mix) => ({
+          fuel: mix.M!.fuel.S!,
+          percentage: parseFloat(mix.M!.percentage.N!),
+        })),
+      });
+    });
+  }
+  return results;
 }
