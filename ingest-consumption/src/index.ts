c0099588c32b58e5ee2ba0606c7ed547b67efa5c
Matthew Benton <m@mattb.tech>
2024-06-16 17:30:22 +0100

    Combine mini and direct data

--- ingest-consumption/src/index.ts
+++ ingest-consumption/src/index.ts
@@ -7,20 +7,39 @@ import chunk from "lodash.chunk";
 import { Event, datesFromEvent } from "./event";
 import { ConsumptionPoint } from "./consumptionPoint";
 import { fetchMeterMini } from "./meterMini";
+import { fetchMeterDirect } from "./meterDirect";
 
 const DYNAMO_CLIENT = new DynamoDBClient({});
 const CHUNK_SIZE = 25;
 
 export async function handler(event: Event) {
   const { from, to } = datesFromEvent(event);
+  console.log("Fetching consumption data", { from, to });
+  const [mini, direct] = await Promise.all([
+    fetchMeterMini({ from, to }),
+    fetchMeterDirect({ from, to }),
+  ]);
+  console.log("Fetched consumption data", {
+    miniCount: mini.length,
+    directCount: direct.length,
+  });
+  // Passing direct first means that direct data will be preferred to mini data
+  const data = dedupe([...direct, ...mini]);
+  console.log("Deduped consumption data", { count: data.length });
+  await writeData(data);
   console.log("Importing consumption data", { from, to });
-  await importDirect({ from, to });
 }
 
-async function importDirect({ from, to }: { from: Date; to: Date }) {
-  const results = await fetchMeterMini({ from, to });
-  console.log(`Fetched ${results.length} data points`);
-  await writeData(results);
+function dedupe(data: Array<ConsumptionPoint>) {
+  const set = new Set<string>();
+  return data.filter((item) => {
+    const key = `${item.energyType}-${item.startDate}`;
+    if (set.has(key)) {
+      return false;
+    }
+    set.add(key);
+    return true;
+  });
 }
 
 async function writeData(data: Array<ConsumptionPoint>) {
